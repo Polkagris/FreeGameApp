316b9c71d0a23f50bf24adee0f8bd0b2dc508fc2
unknown <arne.n.bjerkaas@gmail.com>
2020-07-11 21:22:14 +0200

    Remove issue with throttling

--- src/routes/gameList.js
+++ src/routes/gameList.js
@@ -17,16 +17,60 @@ const getGameList = async () => {
     console.error(error);
   }
 };
-
+// `https://store.steampowered.com/api/appdetails?appids=${gameId}&filters=price_overview&cc=no&l=en`
 const getGameDetails = async (gameId) => {
   return await axios
-    .get(
-      `https://store.steampowered.com/api/appdetails?appids=${gameId}&cc=no&l=en`
-    )
+    .get(`https://store.steampowered.com/api/appdetails?appids=${gameId}`)
     .then((res) => {
-      if (res.data[gameId].success == false) {
-        throw "Game detail fetch failure";
+      if (!res.data[gameId].data) {
+        console.log(
+          "@@@@@@@@@@ SKIPPED - because of no data in object @@@@@@@@@@",
+          res.data
+        );
+        return;
       }
+      if (res.data[gameId].success == false) {
+        console.error;
+        throw "Game detail fetch failure";
+        //return here too?
+      }
+      console.log(
+        "RETURNING data[gameId] from getGameDetails fetch call.",
+        res.data[gameId].data.steam_appid,
+        " TYPE: " + res.data[gameId].data.type
+      );
+
+      if (!res.data[gameId].data.header_image) {
+        console.log(
+          res.data[gameId].data.steam_appid +
+            "IS MISSING SOME DETAILS - header_image"
+        );
+      } else if (!res.data[gameId].data.name) {
+        console.log(
+          res.data[gameId].data.steam_appid + "IS MISSING SOME DETAILS - name"
+        );
+      } else if (!res.data[gameId].data.price_overview) {
+        console.log(
+          res.data[gameId].data.steam_appid +
+            "IS MISSING SOME DETAILS - price_overview"
+        );
+      } else if (!res.data[gameId].data.short_description) {
+        console.log(
+          res.data[gameId].data.steam_appid +
+            "IS MISSING SOME DETAILS - short_description"
+        );
+      } else if (!res.data[gameId].data.is_free) {
+        console.log(
+          res.data[gameId].data.steam_appid +
+            "IS MISSING SOME DETAILS - is_free"
+        );
+      } else if (!res.data[gameId].data.genres[0].description) {
+        console.log(
+          res.data[gameId].data.steam_appid +
+            "IS MISSING SOME DETAILS - description"
+        );
+      }
+
       return res.data[gameId];
     })
     .catch(console.error);
@@ -55,37 +99,48 @@ router.route("/").post(async (req, res) => {
   // Total calls = 94599 + 1
   let canStillRun = true;
 
-  // console.log("NEW GAME FROM MAP:", newGameFromMap);
   let timeOutVariable = 0;
 
-  const gameDetailFromMap = newGameFromMap.slice(0, 99).map(async (game) => {
-    timeOutVariable += 150;
-    setTimeout(async () => {
-      const gameDetailId = game.gameId;
-      console.log("&&&&&& gameDetailId &&&&&&", gameDetailId);
-      // gameId.data -> only if gameId.success == true
-      if (canStillRun === true) {
-        console.log("GAME ID: ", game);
-        const gameDetail = await getGameDetails(gameDetailId);
-        if (!gameDetail) {
-          canStillRun = false;
-          return;
-        }
-        return new Game({
-          gameId: gameDetailId,
-          gameImage: gameDetail.data.header_image,
-          gameName: gameDetail.data.name,
-          gamePrice: gameDetail.data.price_overview
-            ? gameDetail.data.price_overview.final / 100
-            : null,
-          gameDescription: gameDetail.data.short_description,
-          gameIsFree: gameDetail.data.is_free,
-          gameGenres: gameDetail.data.genres[0].description,
-        }).save();
-      }
-    }, timeOutVariable);
+  let gameIdArray = [];
+  newGameFromMap.slice(333, 352).map(async (game) => {
+    gameIdArray.push(game.gameId);
   });
-  // Game.insertMany(gameDetailFromMap);
+
+  const gameDetailFromMap = newGameFromMap.slice(389, 409).map(async (game) => {
+    timeOutVariable += 50;
+    // setTimeout(async () => {
+    const gameDetailId = game.gameId;
+
+    console.log("&&&&&& game ID &&&&&&", gameDetailId);
+    // gameId.data -> only if gameId.success == true
+    if (canStillRun === true) {
+      console.log("GAME: ", game);
+      // Try my best!
+      console.log(
+        "---------------------------- GameIdArray ----------------------------:",
+        gameIdArray
+      );
+      // gameDetailId
+      const gameDetail = await getGameDetails(gameDetailId);
+      if (!gameDetail) {
+        canStillRun = false;
+        return;
+      }
+      return new Game({
+        gameId: gameDetailId,
+        gameImage: gameDetail.data.header_image,
+        gameName: gameDetail.data.name,
+        gamePrice: gameDetail.data.price_overview
+          ? gameDetail.data.price_overview.final / 100
+          : null,
+        gameDescription: gameDetail.data.short_description,
+        gameIsFree: gameDetail.data.is_free,
+        gameGenres: gameDetail.data.genres[0].description,
+      }).save();
+    }
+    // }, timeOutVariable);
+  });
+  await Game.insertMany(gameDetailFromMap);
   console.log("GAME DETAILS FROM MAP:", gameDetailFromMap);
   res.json("List of games successfully updated.");
 });
